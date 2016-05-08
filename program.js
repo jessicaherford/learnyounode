cafab0bbc032c5cf86b38c248eecdc72162dcca5
JessicaHerford <jrherford12@gmail.com>
2016-05-08 15:54:21 -0600

    Make it modular passed

--- program.js
+++ program.js
@@ -1,15 +1,15 @@
-var filemodule = require("./modules.js");
+var filterFunc = require("./modules.js");
 var dir = process.argv[2];
 var stringFilterFunc = process.argv[3];
 
 filterFunc(dir, stringFilterFunc, function(err,files){
-    if(err){
-        return console.error("There was an error: ", err);
+    if(err)
+        return console.error("There was an error: ", err)
 
     files.forEach(function(file){
       console.log(file);
     })
-    }
+
 });
 
 
